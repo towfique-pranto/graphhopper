1a24b6d443f483ffde514c3d1c601ac92332b29d
towfique-pranto <towfique.pranto@gmail.com>
2026-03-08 11:18:38 +0000

    Add routing functionality to geocoding results

--- graphhopper_parse-json_1.py
+++ graphhopper_parse-json_1.py
@@ -57,7 +57,21 @@ while True:
     if loc2 == "quit" or loc2 == "q":
         break
     dest = geocoding(loc2, key)
-    print(dest)
+    print("=================================================")
+    if orig[0] == 200 and dest[0] == 200:
+        op="&point="+str(orig[1])+"%2C"+str(orig[2])
+        dp="&point="+str(dest[1])+"%2C"+str(dest[2])
+        paths_url = route_url + urllib.parse.urlencode({"key":key}) + op + dp
+        paths_status = requests.get(paths_url).status_code
+        paths_data = requests.get(paths_url).json()
+        print("Routing API Status: " + str(paths_status) + "\nRouting API URL:\n" + paths_url) 
+    print("=================================================")
+    print("Directions from " + orig[3] + " to " + dest[3])
+    print("=================================================")
+    if paths_status == 200:
+        print("Distance Traveled: " + str(paths_data["paths"][0]["distance"]) + "m")
+        print("Trip Duration: " + str(paths_data["paths"][0]["time"]) + "millisec")
+        print("=================================================") 
 
 orig = geocoding(loc1, key)
 print(orig)
